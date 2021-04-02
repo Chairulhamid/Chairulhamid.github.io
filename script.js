3220f5ce85e5aa7ecdaca915c1c737ce5b661a21
Chairulhamid <chairulhamid01@gmail.com>
2021-04-02 21:58:27 +0700

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//untuk scrool header
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
@@ -9,3 +10,31 @@ function menuToggle() {
   navigation.classList.toggle("active");
   menuToggle.classList.toggle("active");
 }
+
+//untuk pesan kontak form
+const scriptURL = "https://script.google.com/macros/s/AKfycbwGvbPfJEpOQRq_Jpik3KxrcyAOJJqXBMudmnSPmN1b5RDLOyXrobElMvwKAqwOwD3Crw/exec";
+const form = document.forms["contact-form-chairulhamid"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //ketika tombol submit di kirm
+  //tampil tombol loading
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      //ketika tombol submit di kirm
+      //hialngkan tombol loading
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // tampil alert
+      myAlert.classList.toggle("d-none");
+      //reset form
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
